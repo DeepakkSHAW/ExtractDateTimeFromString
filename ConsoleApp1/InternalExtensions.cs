37c47bdfb37ccad258b79e7e0ebcf24f47e6dbe8
Deepak Shaw <Deepak.Shaw@chisholm.edu.au>
2019-05-28 11:25:05 +1000

    Final test done

---
+++ ConsoleApp1/InternalExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtractDateTimeFromString
+{
+    internal static class InternalExtensions
+    {
+        internal static bool IsDateType(this string o)
+        {
+            return DateTime.TryParse(o, out DateTime dummy);
+        }
+        internal static bool IsNumericType(this string o)
+        {
+            return double.TryParse(o, out double dummy);
+        }
+        internal static bool IsWeekend(this DateTime date)
+        {
+            return new[] { DayOfWeek.Sunday, DayOfWeek.Saturday }.Contains(date.DayOfWeek);
+        }
+        internal static DateTime PreviousWorkDay(this DateTime date)
+        {
+            do
+            {
+                date = date.AddDays(-1);
+            }
+            while (IsWeekend(date));
+
+            return date;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace ExtractDateTimeFromString
 {
@@ -7,66 +6,52 @@ namespace ExtractDateTimeFromString
     {
         static void Main(string[] args)
         {
+            //**initialize IST**//
+            TimeZoneInfo INDIAN_ZONE = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+            DateTime tm = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, INDIAN_ZONE);
+
+            DateTime dt = DateTime.Today;
+            string sDate = string.Empty, sTime = string.Empty;
+
+            //** dummy data for test**//
             string[] inputString = {
-                "Closing >> At close: May 24 3:29PM IST",
-                "Opening >> As of  9:23AM IST. Market open.",
+                "At close: May 25 3:29PM IST",
+                "At close: May 27 3:30PM IST",
+                "At close: 3:30PM IST",
+                "As of  9:23AM IST. Market open.",
                 "As of May 27 9:30AM IST. Market open.",
                 "As of May 27 9:26AM IST. Market open.",
                 "As of May 24 3:52PM IST. Market open."
             };
-            foreach (string sin in inputString)
-                Console.WriteLine(sin);
-            var splits = inputString[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string sin in inputString) Console.WriteLine(sin);
+            var inputValue = inputString[0].Trim();
+            //** ---------------------**//
 
-            string sDate = string.Empty, sTime = string.Empty;
-            for (int i = 0; i < splits.Length; i++)
+            var splits = inputValue.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var index = Array.FindIndex(splits, x => x.ToUpper() == "IST");
+            if (index > -1)
             {
-                if (Int32.TryParse(splits[i], out int value))
-                {
-                    sDate = $"{splits[i]} {splits[i - 1]}";
-                    sTime = $"{splits[i+1]}".Insert($"{splits[i + 1]}".Length-2," ");
-                }
+                if (index - 1 > -1) sTime = $"{splits[index - 1]}";
+                if (index - 3 > -1) sDate = $"{splits[index - 2]} {splits[index - 3]}";
             }
-            DateTime dt = DateTime.ParseExact(sDate, "dd MMM", CultureInfo.InvariantCulture);
-           // DateTime tm = DateTime.ParseExact(sTime, "h:mm tt", CultureInfo.InvariantCulture);
-            DateTime tm = (DateTime)Convert.ChangeType(sTime, typeof(DateTime));
+
+            //** precaution in case missing date & time**//
+            if (sDate.IsDateType())
+                dt = (DateTime)Convert.ChangeType(sDate, typeof(DateTime));
+            if(sTime.IsDateType())
+                tm = (DateTime)Convert.ChangeType(sTime, typeof(DateTime));
+
+            //** adjust the date if date in missing in the downloaded time stamp**//
             var vReturn = dt.Date.Add(tm.TimeOfDay);
+            vReturn = vReturn > TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, INDIAN_ZONE) ?
+                vReturn.AddDays(-1) : vReturn;
+
+            //** Convert the downloaded time into last business day**//
+            if (vReturn.IsWeekend())
+                vReturn = vReturn.PreviousWorkDay();
 
             Console.WriteLine(vReturn.ToString());
             Console.ReadKey();
         }
     }
 }
-
-//DateTime dt = DateTime.ParseExact(theDateTime.ToString().Trim(), "dd MMM", CultureInfo.InvariantCulture);
-//DateTime tm = DateTime.ParseExact(theDateTime.ToString().Trim(), "HH:mm:ss", CultureInfo.InvariantCulture);
-//sp.ValueOn = dt.Date.Add(tm.TimeOfDay);
-
-#region dummy
-//string s = inputString[2];
-//string subs = s.Substring(0,s.IndexOf("IST")-1);
-
-//var stime = subs.Substring(subs.LastIndexOfAny(new char[] {' '}) + 1,
-//    (subs.Length - subs.LastIndexOfAny(new char[] { ' ' }) -1)
-//    );
-//Console.WriteLine(stime);
-
-//var sdt = subs.Remove(subs.IndexOf(stime), subs.Length - subs.IndexOf(stime)).Split(' ',StringSplitOptions.RemoveEmptyEntries);
-//var temp = string.Empty;
-//for (int i = 0; i< sdt.Length; i++)
-//{
-//    bool success = Int32.TryParse(sdt[i], out int value);
-//    if (success)
-//    {
-//        temp = $"{sdt[i]} {sdt[i - 1]}";
-//        //DateTime dd = DateTime.ParseExact(sdt[i], "dd", CultureInfo.InvariantCulture);
-//        //DateTime mm = DateTime.ParseExact(sdt[i-1], "MMM", CultureInfo.InvariantCulture);
-//        break;
-//    }
-//}
-////subs = subs.Substring(subs.LastIndexOf(' ', subs.LastIndexOf(' ') + 1), 1); 
-//DateTime dt = DateTime.ParseExact(temp.ToString().Trim(), "dd MMM", CultureInfo.InvariantCulture);
-//DateTime tm = DateTime.ParseExact(stime.ToString().Trim(), "HH:mm:ss", CultureInfo.InvariantCulture);
-//var vReturn = dt.Date.Add(tm.TimeOfDay);
-//Console.WriteLine(subs);
-#endregion
